d34480216dbb62055413f4ecdcabdaa12934386a
Konsta Purtsi <konsta@purtsi.fi>
2025-10-27 13:03:33 +0200

    Add a hero with some basic styles

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,ts,tsx,md,mdx}"],
+  theme: {
+    colors: {
+      salmon: "#f88e79",
+      pale: "#fcdbd5",
+      purple: "#6b2d5c",
+      licorice: "#1f1300",
+      white: "#fff",
+      black: "#000",
+    },
+    fontFamily: {
+      sans: ['Anuphan', 'sans-serif'],
+      display: ['Chicle', 'serif'],
+    }
+  }
+};
